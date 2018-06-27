7f130937844591c269bc2ce7e88a163729943a3e
Sharyn <sharyn@nonacreative.com>
2018-06-27 15:33:33 +0200

    Changing css path

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" href="/_next/static/style.css" />
+          <link rel="stylesheet" href="_next/static/style.css" />
           <link rel="stylesheet" type="text/css" charset="UTF-8" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         </Head>
