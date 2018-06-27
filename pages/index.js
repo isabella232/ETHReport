581ee05ab53737e2a38be918e33cf4058ce89ca4
deploy <deploy@nonacreative.com>
2018-06-27 15:12:31 +0200

    Deploy github pages

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles.scss';
-
-export default () => (
-  <div className="home page-wrapper">
-    <h1><strong>ETHPrize website</strong> <br />coming soon!</h1>
-  </div>
-);
