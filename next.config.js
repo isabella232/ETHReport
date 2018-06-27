be18958119e80d0d892b5c87a4944f49e24f8728
deploy <deploy@nonacreative.com>
2018-06-27 14:10:38 +0200

    Release the Kraken!

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withSass = require('@zeit/next-sass')
-module.exports =
-  withSass({
-    exportPathMap: function(defaultPathMap) {
-      return {
-        '/': { page: '/' }
-      }
-    }
-  });
