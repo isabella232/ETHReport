c79e58118f76382c78e69e805741fdd57a28b432
Sharyn <sharyn@nonacreative.com>
2018-07-06 13:47:25 +0200

    Adding search results component

---
+++ components/searchResults/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+const SearchResults = props => (
+  <div className="search-results">
+    <p>Search results go here</p>
+  </div>
+);
+
+export default SearchResults;
