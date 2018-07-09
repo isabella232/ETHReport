f8bedec3ea77476fdfd9adfba5f7eb7ab2dbb0bd
James Gareth Smith <james.gareth.smith@gmail.com>
2018-07-09 13:40:21 +0200

    hardcoded repo to get staging working for client review

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="_next/static/style.css" />
-          <link rel="stylesheet" type="text/css" charset="UTF-8" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+          <link rel="stylesheet" type="text/css" charSet="UTF-8" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
           <link href="//fonts.googleapis.com/css?family=Roboto+Mono:400,700" rel="stylesheet" />
         </Head>
