1a6179e9319568b6777c6a1c676781bec6ae1d54
James Gareth Smith <james.gareth.smith@gmail.com>
2018-07-09 09:42:19 +0200

    set asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withSass = require('@zeit/next-sass')
 module.exports =
   withSass({
+    assetPrefix: isProduction ? '/ETHPrize' : '',
     exportPathMap: function(defaultPathMap) {
       return {
         '/': { page: '/' }
