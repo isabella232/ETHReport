d3525939e1d46f954b4fa853a2a66eeb1b807b80
Sharyn <sharyn@nonacreative.com>
2018-07-18 12:10:04 +0200

    extracting content into data object

--- components/contentBlurb/index.js
+++ components/contentBlurb/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import Data from '../../data/content/content';
+import Parser from 'html-react-parser';
 import './style.scss';
 
 const ContentBlurb = () => (
   <div className="content-blurb">
     <div className="container">
-      <p>
-        The community behind ETHPrize has interviewed over 100 developers in order to produce this
-        website which showcases the biggest frustrations and opportunities in the ecosystem.
-      </p>
-      <p>
-        We invite all developers, product managers, designers and #buidlers to use the ideas here
-        and figure out what to make next so that we can all continue to build the world’s best
-        cryptocurrency community and ship great products that actually improve the lives of as many
-        people as possible.
-      </p>
+      { Parser(Data.blurb) }
     </div>
   </div>
 );
