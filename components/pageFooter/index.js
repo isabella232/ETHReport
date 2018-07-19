3f66059f00fdc59237a7fb88396c5a9531b32b3e
Sharyn <sharyn@nonacreative.com>
2018-07-19 11:47:11 +0200

    Who is Nona?

--- components/pageFooter/index.js
+++ components/pageFooter/index.js
@@ -6,7 +6,7 @@ const PageFooter = () => (
     <div className="container">
       <p>
         Sponsored by <a href="//status.im" target="_blank" rel="noopener noreferrer">Status</a>.
-        Built by <a href="//nona.digital" target="_blank" rel="noopener noreferrer">Nona Creative</a>
+        Built by <a href="//nona.digital" target="_blank" rel="noopener noreferrer">Nona</a>
       </p>
     </div>
   </div>
