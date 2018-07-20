e096341d654f6d0dc3515057ff6b127d3c6d551a
Wisani Shilumani <wisani@Wisanis-MacBook-Pro.local>
2018-07-20 10:35:00 +0200

    Add comment

--- components/browseArchives/index.js
+++ components/browseArchives/index.js
@@ -112,6 +112,8 @@ class BrowseArchives extends React.Component {
     });
   }
 
+
+  // used as an intermediary to give us an easy to search through JSON object
   transformInterviews = (interviews) => {
     const { length } = Object.keys(interviews);
     const betterInterviews = [];
